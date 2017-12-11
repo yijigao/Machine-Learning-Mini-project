fa08602affe0a74b44d538059837eaa68a24b630
yijigao <yijigao92@gmail.com>
2017-12-11 18:52:33 +0800

    preparing for final project

--- ud120-projects/validation/validate_poi.py
+++ ud120-projects/validation/validate_poi.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+import pickle
+import sys
+from tools.feature_format import featureFormat, targetFeatureSplit
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 
 
 """
@@ -10,23 +15,26 @@
     After that, it's not our code anymore--it's yours!
 """
 
-import pickle
-import sys
-sys.path.append("../tools/")
-from feature_format import featureFormat, targetFeatureSplit
+data_dict = pickle.load(open("../final_project/final_project_dataset_unix.pkl", "rb"))
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
-
-### first element is our labels, any added elements are predictor
-### features. Keep this the same for the mini-project, but you'll
-### have a different feature list when you do the final project.
+# first element is our labels, any added elements are predictor
+# features. Keep this the same for the mini-project, but you'll
+# have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
 
-data = featureFormat(data_dict, features_list)
+
+data = featureFormat(data_dict, features_list, sort_keys="../tools/python2_lesson13_keys_unix.pkl")
 labels, features = targetFeatureSplit(data)
 
-
-
-### it's all yours from here forward!  
-
-
+# it's all yours from here forward!
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels,
+                                                                            test_size=0.3, random_state=42)
+#
+# # using Decision tree as classifier
+clf = DecisionTreeClassifier()
+# # training
+# clf.fit(features, labels)
+clf.fit(features_train, labels_train)
+# # # Score
+# print("Test score:", clf.score(features, labels))
+print("Test score:", clf.score(features_test, labels_test))
